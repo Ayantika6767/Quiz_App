beec6d5bcf4ecb05f6505e5d2ce23590f1c5ff5c
Ayantika6767 <77482223+Ayantika6767@users.noreply.github.com>
2022-09-11 13:40:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,58 +34,51 @@ const quizData=[
   },
   ];
   
-  const quiz=document.getElementById('quiz');
-  const answerElements=document.querySelectorAll('.answer');
-  const questionElements=document.getElementById('question');
-  const a_text=document.getElementById('a_text');
-  const b_text=document.getElementById('b_text');
-  const c_text=document.getElementById('c_text');
-  const d_text=document.getElementById('d_text');
-  const submitBtn=document.getElementById('submit');
-  
-  let score=0;
-  let currentQuiz=0;
-  
-  loadQuiz();
-  function loadQuiz()
-  {
-    deSelectAnswers();
-    const currentQuizData=quizData[currentQuiz];
-    questionElements.innerText=currentQuizData.question;
-    a_text.innerText=currentQuizData.a;
-    b_text.innerText=currentQuizData.b;
-    c_text.innerText=currentQuizData.c;
-    d_text.innerText=currentQuizData.d;
-  }
-  
-  function getSelected()
-  {
-    let answer=undefined;
-    answerElements.forEach((answerEl)=>{
-      if(answerEl.checked){
-        answer=answerEl.id;
-      }
-    });
-    return answer;
-  }
-  
-  function deSelectAnswers()
-  {
-    answerElements.forEach((answerEl)=>{
-      answerEl.checked=false;
-    });
-  }
-  
-  submitBtn.addEventListener("click",()=>{
-    const answer=getSelected();
-    if(answer && answer===quizData[currentQuiz].correct){
+  const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let score = 0;
+let currentQuiz = 0;
+loadQuiz();
+function loadQuiz() {
+  deselectAnswer();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+function getSelected() {
+  let answer = undefined;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+function deselectAnswer() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
       score++;
     }
     currentQuiz++;
-    if(currentQuiz<quizData.length){
+    if (currentQuiz < quizData.length) {
       loadQuiz();
-    }
-   else {
+    } else {
       quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions. </h2> <button onclick="location.reload()">Reload</button>`;
     }
   }
